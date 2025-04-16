503f9026432822f6b82d876a50e2977c2d12a057
Reza Faraji <r3zafaraji@gmail.com>
2025-04-16 15:38:13 +0200

    add dark light theme button

--- 01-making-vue-alive/index.js
+++ 01-making-vue-alive/index.js
@@ -1,8 +1,16 @@
 const {createApp, ref, computed} = Vue;
 const App = {
     setup() {
+        // constants
+        const inputClasses = "block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 " +
+            "text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 " +
+            "dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500";
+
+        // vars
         let message = ref('-');
         let age = ref(null);
+
+        // computed
         let canDrive = computed(() => {
             if (!age.value) message.value = '-';
             else if (age.value >= 18) message.value = 'Congratulations! You are ready to take a course!';
@@ -10,11 +18,18 @@ const App = {
 
             return age.value >= 18
         });
+
         return {
             message,
             canDrive,
-            age
+            age,
+            inputClasses,
         }
     },
 };
 createApp(App).mount('#app');
+
+function modeSwitch() {
+    document.body.classList.toggle('dark');
+    console.log('##');
+}
