8311b88e132956d563fa6c5d77bcb04ac0702e2c
Reza Faraji <r3zafaraji@gmail.com>
2025-04-21 19:51:51 +0200

    first expremental app finished

--- 01-making-vue-alive/index.js
+++ 01-making-vue-alive/index.js
@@ -1,11 +1,28 @@
-const {createApp, ref, computed} = Vue;
+const { createApp, ref, computed } = Vue;
+
 const App = {
     setup() {
         // constants
-        const inputClasses = "block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-100 " +
-            "text-base focus:ring-blue-500 focus:border-blue-500 focus:bg-blue-100 dark:bg-gray-700 dark:border-gray-600 " +
-            "dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500";
+        const textInput = [
+            "block", "w-full", "p-4", "border", 
+            "border-gray-300", "text-gray-900",
+            "bg-gray-100", "rounded-lg", "text-base",
+            "focus:ring-blue-500", "focus:border-blue-500", 
+            "focus:bg-blue-100", "dark:bg-gray-700",
+            "dark:border-gray-600", "dark:placeholder-gray-400",
+            "dark:text-white", "dark:focus:ring-blue-500", "dark:focus:border-blue-500"
+        ];
 
+        const labelClasses = [
+            "text-base", "text-lg/4", "font-medium", 
+            "text-gray-900", "dark:text-white"
+        ];
+
+        const resultHeadingClasses = [
+            "text-base", "text-lg/4", "font-medium", 
+            "text-gray-500", "dark:text-gray-400"
+        ];
+        
         // vars
         let message = ref('-');
         let age = ref(null);
@@ -15,16 +32,24 @@ const App = {
             if (!age.value) message.value = '-';
             else if (age.value >= 18) message.value = 'Congratulations! You are ready to take a course!';
             else message.value = 'You are not ready to take a course!';
-
-            return age.value >= 18
+            return age.value >= 18;
         });
+
+        let resultTextClasses = computed(() => (
+            canDrive.value ? 
+            ['text-green-700', 'dark:text-green-500'] : 
+            ['text-red-600', 'dark:text-red-400']
+        ));
 
         return {
             message,
             canDrive,
             age,
-            inputClasses,
-        }
+            textInput,
+            labelClasses,
+            resultHeadingClasses,
+            resultTextClasses
+        };
     },
 };
 createApp(App).mount('#app');
@@ -61,3 +86,31 @@ function modeSwitch() {
         }
     });
 })();
+
+
+const bodyClasses = [
+    "min-h-screen", "min-w-screen", "flex", 
+    "flex-col", "justify-start", "items-start", 
+    "bg-white", "dark:bg-gray-800", "text-gray-900", 
+    "dark:text-white"
+];
+
+const navClasses = [
+    "bg-gray-100", "dark:bg-gray-700", "w-full", 
+    "px-12", "py-6", "flex", "justify-between", 
+    "items-center", "shadow-md"
+];
+
+const modeBtnClasses = [
+    "h-12", "w-12", "rounded-lg", "p-2", 
+    "text-violet-700", "hover:bg-violet-700", "hover:text-gray-100", 
+    "dark:text-yellow-500", "dark:hover:bg-yellow-500", "dark:hover:text-gray-700", 
+    "transition", "duration-200", "ease-in-out"
+];
+
+
+document.querySelector('.body').classList.add(...bodyClasses);
+document.querySelector('.nav').classList.add(...navClasses);
+document.querySelector('.mode-btn').classList.add(...modeBtnClasses);
+
+
